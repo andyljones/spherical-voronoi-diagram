bd6c902865b09dc896c4443d93dcec5f7ca7b1d2
Andy Jones <andyjones.ed@gmail.com>
2013-12-27 15:01:02 +0000

    Eliminated much of the trig from EllipseDrawer

--- Assets/Source/EllipseDrawer.cs
+++ Assets/Source/EllipseDrawer.cs
@@ -18,16 +18,12 @@ public static class EllipseDrawer
 
     public static Vector3 PointOnEllipse(Vector3 focus, float height, float longitude)
     {
-        var longitudeOfFocus = Mathf.Atan2(-focus.y, focus.x);
-
         var Y = focus.z - height;
-        var X = Mathf.Sqrt(1-height*height) - Mathf.Sqrt(1-focus.z*focus.z)*Mathf.Cos(longitudeOfFocus - longitude);
+        var X = Mathf.Sqrt(1-height*height) - (focus.x * Mathf.Cos(longitude) - focus.y * Mathf.Sin(longitude));
 
-        var colatitude = Mathf.Atan2(Y, X);
-
-        var x = Mathf.Sin(colatitude)*Mathf.Cos(longitude);
-        var y = -Mathf.Sin(colatitude)*Mathf.Sin(longitude);
-        var z = Mathf.Cos(colatitude);
+        var x = Y/Mathf.Sqrt(X*X+Y*Y) * Mathf.Cos(longitude);
+        var y = Y/Mathf.Sqrt(X*X+Y*Y) * -Mathf.Sin(longitude);
+        var z = X/Mathf.Sqrt(X*X+Y*Y);
 
         return new Vector3(x, y, z);
     }
